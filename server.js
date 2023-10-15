8ca9b519c3207c56ae3f0cbdaff8b7aabce2af57
Vicky Poonia <114798756+vicky-poonia@users.noreply.github.com>
2023-10-15 22:51:02 +0530

    read all the messages from the file and show it at the top of the form

--- server.js
+++ server.js
@@ -49,45 +49,9 @@
 
 
 const http = require("http");
-const fs = require("fs");
+const rou = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><Button type="submit">SEND</Button></form>');
-
-    // Add this placeholder to display the message
-    const message = fs.readFileSync("message.txt", "utf8");
-    console.log("message", message);
-    if (message) {
-      console.log('kjfndijvjk')
-      res.write(`<div>Message: ${message}</div>`);
-    }
-
-    res.write("</body></html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (c) => {
-      body.push(c);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message, ()=>{
-        res.setHeader("Location", "/");
-        res.statusCode = 302;
-        res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(rou);
 
 server.listen(4500, "localhost", () => {
   console.log("listening...");
